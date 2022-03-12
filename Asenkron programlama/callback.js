c8cdca305894f18c0f9fa2db55c266d054c74dad
Hüseyin Topal <huseyin05topal@gmail.com>
2022-03-12 09:07:46 +0300

    example done

---
+++ Asenkron programlama/callback.js
@@ -0,0 +1,48 @@
+const todos = [
+    {title : "Todo 1", description: "ingilizce çalışılacak.." },
+    {title : "Todo 2", description: "react kurulum tamamla.." },
+    {title : "Todo 3", description: "Meyve alınacak.." }
+];
+
+let todoListEl = document.getElementById("todoList");
+
+function todoList(){
+    setTimeout(() => {
+        let todoItems = ""       
+        todos.forEach(item => {
+            todoItems += `
+            <li>
+                <b>${item.title}</b>
+                <p>${item.description}</p>            
+            </li>`
+            // console.log(todoItems);
+        })
+        todoListEl.innerHTML = todoItems;
+    }, 1000);
+}
+
+function newTodo(todo, callback){
+    setTimeout(()=>{
+        todos.push(todo);
+        callback();
+    }, 2000);
+}
+newTodo({
+    title : "Todo 4",
+    description : "Garaj düzenlenip temizlenecek.."
+}, todoList)
+
+todoList();
+console.log(todos);
+
+// export default function () {
+//     console.log('We are all animals!');
+// }
+
+// export function cat() {
+//     console.log('Meeeow!');
+// }
+
+// export function dog() {
+//     console.log('Rufff!');
+// }
